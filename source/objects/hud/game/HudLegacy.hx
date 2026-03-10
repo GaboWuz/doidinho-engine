5dd37ebe6521db7eeb41565112ac96abeca4c8c0
StarNovaBR <cherrylegal001@gmail.com>
2026-03-10 17:02:06 -0300

    Downscroll esta bom, falta o upscroll

--- source/objects/hud/game/HudLegacy.hx
+++ source/objects/hud/game/HudLegacy.hx
@@ -148,11 +148,11 @@ class HudLegacy extends HudClass
 	{
 		super.updatePositions();
 		healthBar.x = (FlxG.width / 2) - (healthBar.border.width / 2) - 200;
-		healthBar.y = (downscroll ? 70 - 60 : FlxG.height - healthBar.border.height - 50 - 80);
+		healthBar.y = (downscroll ? 70 - 60 : FlxG.height - healthBar.border.height - 50 - 120);
 		vidar.y = (downscroll ? 100 - 70 : 100 + 450);
 		
 		updateInfoTxt();
-		infoTxt.y = (downscroll ? healthBar.y + healthBar.border.height - 15 : healthBar.y + healthBar.border.height + 30);
+		infoTxt.y = (downscroll ? healthBar.y + healthBar.border.height - 15 : healthBar.y + healthBar.border.height + 50);
 		
 		badScoreTxt.y = healthBar.y - badScoreTxt.height - 4;
 		
