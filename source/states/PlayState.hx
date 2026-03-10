c102e7c9fbccf8d801ea863c4a802db210a6746d
StarNovaBR <cherrylegal001@gmail.com>
2026-03-09 21:12:24 -0300

    ok entao, AQUI VAI

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -276,6 +276,9 @@ class PlayState extends MusicBeatState
 			case "useless"|"beep-power":
 				hudBuild = new HudOG();
 				hudBuild.alpha = 0.0;
+			case "lapoluz" | "faker-identity":
+				hudBuild = new HudLegacy();
+				hudBuild.alpha = 0.0;
 			default:
 				hudBuild = new HudDoido();
 				hudBuild.alpha = 0.0;
