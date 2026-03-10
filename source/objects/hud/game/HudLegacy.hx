9e634976c0e5be215c6dc06455b09c753e7c716f
StarNovaBR <cherrylegal001@gmail.com>
2026-03-10 17:29:25 -0300

    ata, agora vai

--- source/objects/hud/game/HudLegacy.hx
+++ source/objects/hud/game/HudLegacy.hx
@@ -148,11 +148,11 @@ class HudLegacy extends HudClass
 	{
 		super.updatePositions();
 		healthBar.x = (FlxG.width / 2) - (healthBar.border.width / 2) - 200;
-		healthBar.y = (downscroll ? 70 - 59 : FlxG.height - healthBar.border.height - 50 - 119);
+		healthBar.y = (downscroll ? 70 - 57 : FlxG.height - healthBar.border.height - 50 - 118);
 		vidar.y = (downscroll ? 100 - 70 : 100 + 450);
 		
 		updateInfoTxt();
-		infoTxt.y = (downscroll ? healthBar.y + healthBar.border.height - 15 : healthBar.y + healthBar.border.height + 100);
+		infoTxt.y = (downscroll ? healthBar.y + healthBar.border.height - 15 : healthBar.y + healthBar.border.height + 130);
 		
 		badScoreTxt.y = healthBar.y - badScoreTxt.height - 4;
 		
