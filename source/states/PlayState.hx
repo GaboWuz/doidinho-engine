6c4b001c1503b74c793c3dc724320dac98ce85f8
StarNovaBR <cherrylegal001@gmail.com>
2026-03-15 18:27:22 -0300

    Depois eu ajeito

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -334,7 +334,7 @@ class PlayState extends MusicBeatState
 		add(hudBuild);
 		
 		// song title
-		titleText = new FlxText(0, 301, 1000, SongData.formatSong(SONG.song), 50);
+		titleText = new FlxText(0, 301, 1000, SONG.song, 50);
 	    titleText.setFormat(Paths.font('vcr.ttf'), 50, FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 	    titleText.borderSize = 2;
 	    titleText.cameras = [camOther];
