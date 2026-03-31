9b292fcb874debc05ffdc337836e7588310c7d46
StarNovaBR <cherrylegal001@gmail.com>
2026-03-31 17:14:29 -0300

    opa

--- source/objects/hud/game/HudMoogus.hx
+++ source/objects/hud/game/HudMoogus.hx
@@ -215,7 +215,7 @@ class HudMoogus extends HudClass
 		
 		timeTxt.visible = false;
 		
-		var songPercent:Float = Conductor.songPos / PlayState.songLength - 0.01;
+		var songPercent:Float = Conductor.songPos / PlayState.songLength - 0.03;
 		
 		timer3.scale.set(1.168 * songPercent, 0.035);
 		timer3.updateHitbox();
