8a6366874a5cfb45b8081e584eff1b04cd59674a
StarNovaBR <cherrylegal001@gmail.com>
2026-04-09 14:08:07 -0300

    ta quase

--- source/states/menu/MainMenuState.hx
+++ source/states/menu/MainMenuState.hx
@@ -109,7 +109,7 @@ class MainMenuState extends MusicBeatState
 		grpOptions.members[0].x -= 440;
 		grpOptions.members[0].y += 50;
 		grpOptions.members[1].x -= 150;
-		grpOptions.members[1].y += 70;
+		grpOptions.members[1].y += 80;
 		
 		var doidoSplash:String = 'Doido Engine ${lime.app.Application.current.meta.get('version')}';
 		var funkySplash:String = 'Friday Night Funkin\' Rewritten';
