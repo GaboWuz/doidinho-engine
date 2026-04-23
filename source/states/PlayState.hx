cb848700d2000a3b4d89096cec9c7766902d7d1e
StarNovaBR <cherrylegal001@gmail.com>
2026-04-23 20:36:19 -0300

    chorei

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1783,11 +1783,10 @@ class PlayState extends MusicBeatState
 		syncSong();
 		
 		if (SONG.song == "sixxey" && curStep == 482) { //brooo chilll
-		    gf.playAnim('walkLeft', true);
 			FlxTween.tween(gf.char, {x: -250}, 1.3, {
 			    ease: FlxEase.linear,
 			    onComplete: function(twn:FlxTween) {
-			        gf.playAnim('idle');
+			        gf.char.playAnim('idle');
 			    }
 			});
 		}
